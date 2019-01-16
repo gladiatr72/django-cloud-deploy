67f50092eb042c5f62ae0aa05f86970af51dd319
Xiaofei Wang <wangxf@google.com>
2019-01-16 06:27:52 -0800

    Fix

--- .circleci/install_gcloud.sh
+++
@@ -1,6 +0,0 @@
-set -e
-set -x
-echo $PATH
-wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-230.0.0-linux-x86_64.tar.gz -O ~/google-cloud-sdk.tar.gz
-tar -xzf ~/google-cloud-sdk.tar.gz -C ~/
-sudo source ~/google-cloud-sdk/install.sh
