412fec03b7328d9d0465778ed6995386142df533
Xiaofei Wang <wangxf@google.com>
2019-01-16 05:53:52 -0800

    Fix

--- .circleci/install_gcloud.sh
+++ .circleci/install_gcloud.sh
@@ -1,6 +1,6 @@
 set -e
 set -x
-export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
-echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt-get update && sudo apt-get install google-cloud-sdk
+echo $PATH
+wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-230.0.0-linux-x86_64.tar.gz -O ~/google-cloud-sdk.tar.gz
+tar -xvzf ~/google-cloud-sdk.tar.gz -C ~/
+source ~/google-cloud-sdk/install.sh
