1d84c2d9d62fe86b3616eae49d81ba32d3295ffd
Enrique Padilla <epad@google.com>
2019-02-26 14:13:10 -0800

    Fix bug when using update command

--- django_cloud_deploy/cli/update.py
+++ django_cloud_deploy/cli/update.py
@@ -16,10 +16,15 @@
 import argparse
 import sys
 
+from django_cloud_deploy import config
 from django_cloud_deploy import tool_requirements
 from django_cloud_deploy.cli import io
 from django_cloud_deploy.cli import prompt
 import django_cloud_deploy.workflow as workflow
+
+
+class InvalidConfigError(Exception):
+    """A error occurred when fail to read required information from config."""
 
 
 def add_arguments(parser):
@@ -43,10 +48,6 @@ def add_arguments(parser):
 
 
 def main(args: argparse.Namespace, console: io.IO = io.ConsoleIO()):
-
-    if not tool_requirements.check_and_handle_requirements(
-            console, args.backend):
-        return
 
     prompt_order = [
         'credentials',
@@ -90,6 +91,21 @@ def main(args: argparse.Namespace, console: io.IO = io.ConsoleIO()):
             step = '<b>[{}/{}]</b>'.format(step + 1, num_steps)
             actual_parameters[parameter_name] = prompter.prompt(
                 console, step, actual_parameters)
+
+    # This got moved from the start because we wanted to save the user from
+    # giving us another bit of information that we can automatically retrieve
+    # It will be rare if they are updating, that they do not have the
+    # requirements.
+    django_dir = actual_parameters['django_directory_path']
+    config_obj = config.Configuration(django_dir)
+    backend = config_obj.get('backend')
+    if not backend:
+        raise InvalidConfigError(
+            'Configuration file in [{}] does not contain enough '
+            'information to update a Django project.'.format(django_dir))
+
+    if not tool_requirements.check_and_handle_requirements(console, backend):
+        return
 
     workflow_manager = workflow.WorkflowManager(
         actual_parameters['credentials'])
